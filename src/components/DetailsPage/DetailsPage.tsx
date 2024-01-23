55708894d8258985c3662b9a4a29c70060e10458
fmaxime <maxime.fuzeau@ynov.com>
2024-01-23 19:10:50 +0100

    :lipstick: | make page details

--- src/components/DetailsPage/DetailsPage.tsx
+++ src/components/DetailsPage/DetailsPage.tsx
@@ -4,8 +4,12 @@ import "./DetailsPage.css";
 export const DetailsPage = () => {
   return (
     <main className="detailsPage">
-      <div className="mainFirstPage">
-        <img src={montre} className="montre" alt="montre" />
+      <img src={montre} className="montreImg" alt="montre" />
+      <div className="triangleDetails"></div>
+      <div className="infosDetails">
+        <h2 className="titleDetails">Marque - Modele</h2>
+        <p className="description">La montre ....</p>
+        <h3 className="price">PRICE</h3>
       </div>
     </main>
   );
